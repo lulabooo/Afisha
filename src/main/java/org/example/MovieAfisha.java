7186d85a9df8f9e976996f79e213218f88b3d538
Maria Loginova <marialog@yandex.ru>
2023-03-07 15:14:36 +0300

    second

---
+++ src/main/java/org/example/MovieAfisha.java
@@ -0,0 +1,36 @@
+package org.example;
+public class MovieAfisha {
+    private String[] movies = new String[0];
+    private int limit;
+
+    public MovieAfisha() {
+        this.limit = 10;
+    }
+    public MovieAfisha(int limit) {
+        this.limit = limit;
+    }
+
+    public void addNew (String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+    public String[] showAll() {
+        return movies;
+    }
+
+    public String[] showLast() {
+        String[] tmp = new String[10];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
+        }
+        return tmp;
+    }
+
+
+
+}
---
+++ src/test/java/MovieAfishaTest.java
@@ -0,0 +1,58 @@
+import org.example.MovieAfisha;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieAfishaTest {
+    @Test
+    public void shouldShowAll() {
+        MovieAfisha afisha = new MovieAfisha();
+        afisha.addNew("One");
+        afisha.addNew("Two");
+        afisha.addNew("Three");
+
+        String[] expected = {"One", "Two", "Three"};
+        String[] actual = afisha.showAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldShowlast() {
+        MovieAfisha afisha = new MovieAfisha();
+        afisha.addNew("One");
+        afisha.addNew("Two");
+        afisha.addNew("Three");
+
+        String[] expected = {"Three", "Two", "One"};
+        String[] actual = afisha.showLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldShowlast10() {
+        MovieAfisha afisha = new MovieAfisha();
+        afisha.addNew("1");
+        afisha.addNew("2");
+        afisha.addNew("3");
+        afisha.addNew("4");
+        afisha.addNew("5");
+        afisha.addNew("6");
+        afisha.addNew("7");
+        afisha.addNew("8");
+        afisha.addNew("9");
+        afisha.addNew("10");
+        afisha.addNew("11");
+
+        String[] expected = {"11","10","9","8","7","6","5","4","3","2"};
+        String[] actual = afisha.showLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+}
+
+
+
